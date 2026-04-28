7d401fc07df534aa941e96252b4d0f302504e5a5
DoctorMachete <85594315+DoctorMachete@users.noreply.github.com>
2026-04-28 22:09:36 +0200

    Fix CapsLock state detection in behavior_force_upper

--- src/behaviors/behavior_force_upper.c
+++ src/behaviors/behavior_force_upper.c
@@ -16,6 +16,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include <zmk/behavior.h>
 #include <zmk/events/keycode_state_changed.h>
 #include <zmk/hid_indicators.h>
+#include <dt-bindings/zmk/hid_indicators.h>
 
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
 
@@ -26,7 +27,7 @@ static int on_force_upper_binding_pressed(struct zmk_behavior_binding *binding,
     /* Read current CapsLock state as reported by the host */
     zmk_hid_indicators_t indicators = zmk_hid_indicators_get_current_profile();
    // bool caps_active = (indicators & ZMK_HID_INDICATORS_CAPSLOCK) != 0;
-    bool caps_active = (indicators & HID_KBD_MODIFIER_LEFT_SHIFT) != 0;
+    bool caps_active = (indicators & HID_INDICATOR_CAPS_LOCK) != 0;
 
     int ret;
 
